a7e599b67c98642b49dd94bcaf7249b61b382086
Malotte Westman Lönne <malotte@malotte.net>
2015-05-04 15:45:12 +0200

    New signal encapsulation functions.

--- src/hex.erl
+++ src/hex.erl
@@ -27,6 +27,7 @@
 -export([auto_join/1, join_async/1, join/1]).
 -export([validate_flags/2]).
 -export([text_expand/2]).
+-export([make_self/1]).
 -export([is_string/1]).
 -export([pp_yang/1]).
 -export([save_yang/2]).
@@ -34,6 +35,8 @@
 -export([event/1, event/2]).
 -export([event_list/0]).
 -export([event_signal/1]).
+-export([signal/5]).
+-export([set_signal_value/2]).
 
 -include("../include/hex.hrl").
 
@@ -128,6 +131,19 @@ event(Signal, Env) ->
     hex_server:event(Signal, Env).
 
 %%
+%% Signal encapsulation
+%%
+signal(Id, Channel, Type, Value, Source) ->
+    #hex_signal {id = Id,
+		 chan = Channel,
+		 type = Type,
+		 value = Value,
+		 source = Source}.
+
+set_signal_value(Signal, Value) ->
+    Signal#hex_signal{value = Value}.
+
+%%
 %% Utility to exand environment "variables" in unicode text
 %% variables are written as ${var} where var is a encoded atom
 %% operating system enviroment is accessed through $(VAR)
@@ -211,6 +227,21 @@ rev_variable(Var) ->
 trim_hd([$\s|Cs]) -> trim_hd(Cs);
 trim_hd([$\t|Cs]) -> trim_hd(Cs);
 trim_hd(Cs) -> Cs.
+
+%%
+%% Utility to create a signal id 
+%%
+make_self(NodeID) ->
+    if NodeID band ?HEX_COBID_EXT =/= 0 ->
+	    %% extended nodeid
+	    ?HEX_COBID_EXT bor 
+		(2#0011 bsl 25) bor 
+		(NodeID band ?HEX_XNODE_ID_MASK);
+       true ->
+	    %% short nodeid
+	    (2#0011 bsl 9) bor (NodeID band 16#7f)
+    end.
+
 
 %%
 %% Library function for option validation:
